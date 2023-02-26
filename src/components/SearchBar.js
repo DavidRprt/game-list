75ebabbd66f39f4b2db4736b32ba381623daa018
David Rapoport <davirapo@gmail.com>
2023-02-26 17:52:29 -0300

    first commi5

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -27,7 +27,7 @@ const SearchBar = () => {
           </button>
           <div className="relative w-full">
             <input
-              type="search"
+              type={searchText.type}
               className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
               placeholder="Find a game..."
               required
